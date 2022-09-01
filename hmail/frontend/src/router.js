ea5c1e60331b1a12671c720763827a3fba36e42e
hahmed1523 <hahmed1523@gmail.com>
2022-09-01 11:56:51 -0400

    Add reload and backbutton functionality

--- hmail/frontend/src/router.js
+++ hmail/frontend/src/router.js
@@ -16,6 +16,25 @@ class Router {
                 history.pushState({section: section}, "", `${section}`);
                 router.render();
         }});
+    
+        // Add back button functionality
+        window.onpopstate = function(event) {
+            if (history.state){
+                const component = router.routes[event.state.section];
+                router.render(component);
+            }   
+        }
+
+        // Add reload button functionality
+        // let current_path = window.location.pathname.substring(1);
+        // console.log(current_path);
+        // if (window.performance.getEntries('navigation')[0].type ==='reload'){
+        //     console.log("hello");
+        //     debugger
+        //     console.log(window.location.pathname.substring(1));
+        //     const component = this.activeRoute();
+        //     router.render(component);
+        // }
     }
         
 
@@ -26,9 +45,9 @@ class Router {
     }
 
 
-    render() {
+    render(comp = null) {
         this.node.innerHTML = "";
-        let component = this.activeRoute();
+        let component = comp || this.activeRoute();
         if (component){
             let content = component.render();
             this.node.appendChild(content);
